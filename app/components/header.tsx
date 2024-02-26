5967904f02e9cc0cb6b7ab5a0e5b52c23bea9443
James Vincent Escartin <javines2403@gmail.com>
2024-02-26 14:23:04 +0800

    first commit

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header(){
   return(
     <header className="flex w-full bg-white shadow-lg justify-center py-6">
-      <h1 className=" text-3xl font-bold">Climate Change</h1>
+      <h1 className=" text-3xl font-bold">Climate Change Test</h1>
     </header>
   )
 }
